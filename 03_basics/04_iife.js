4533bae769c84bcca71dbdfb0e4b4a1f20b03c85
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2024-04-21 11:45:41 +0530

    Added notes

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -6,8 +6,10 @@
     console.log(`DB CONNECTED`);
 })();  //; is used to end the IIFE 
 
-( (name) => {
+( (name) => { // This is a simple IIFE, as no name is been assigned to it.
     console.log(`DB CONNECTED TWO ${name}`);
 } )('hitesh')
 
 //(First paranthesis is for function Defination)(Execution Call)
+
+// To write two IIFE in a single file, then use semicolon (;) to separate both of them. 
