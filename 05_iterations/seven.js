3a4f610e85da5ee3b07f10f422a11248141bb036
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-07-01 23:16:01 +0530

    Map Function

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,7 +1,7 @@
 const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 const newNums = myNumers.map( (num) => { return num + 10}) // Map is also a call back function, just like for each. Which automatically returns value. 
-console.log(newNums);  
+console.log(newNums);    
 
 // Chaining (You can use two-three methods one by one which is known as Chaining)
 
