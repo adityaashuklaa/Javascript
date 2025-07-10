8b0cb724c0f1af3b0df28bd30b2aab09b9a6ab2a
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-07-10 23:17:34 +0530

    Promisified Timeout

--- Test/promises.js
+++ Test/promises.js
@@ -2,13 +2,12 @@ console.log("at the top 1");
 
 function promisifiedTimeout(){
     console.log("function called 3");
-    
+      
     return new Promise(function (resolve) {
         console.log("inside promise callback 4");
         
         setTimeout(function (){
-            console.log("setTimout 5");
-            
+            console.log("setTimout 5");     
             resolve("done babyy!! I'm burnt out.")
         }, 5000)
     })
